e6b986d1ba080421b308161c71104732e2701d23
Salman Sazid <114426936+Salman-Sazid@users.noreply.github.com>
2022-10-12 23:19:21 +0600

    Update RandomCal.java

--- RandomCal.java
+++ RandomCal.java
@@ -1,41 +1,26 @@
-// Java implementation of the approach
 import java.util.*;
 import java.lang.Math;
 
 public class RandomCal {
 
-    // Function to return the next random number
     static int getNum(ArrayList<Integer> v) {
-        // Size of the vector
         int n = v.size();
 
-        // Make sure the number is within
-        // the index range
         int index = (int) (Math.random() * n);
 
-        // Get random number from the vector
         int num = v.get(index);
 
-        // Remove the number from the vector
         v.set(index, v.get(n - 1));
         v.remove(n - 1);
 
-        // Return the removed number
         return num;
     }
-
-    // Function to generate n
-    // non-repeating random numbers
     static void generateRandom(int n) {
         ArrayList<Integer> v = new ArrayList<>(n);
 
-        // Fill the vector with the values
-        // 1, 2, 3, ..., n
         for (int i = 0; i < n; i++)
             v.add(i + 1);
 
-        // While vector has elements
-        // get a random number from the vector and print it
         while (v.size() > 0) {
             System.out.print(getNum(v) + " ");
         }
